38c35db518744a8bbc0fc6b6f825ee24c810cd79
Brendan Landis <b@slownames.net>
2023-11-12 08:45:57 -0500

    desktop nav is feeling better

--- app/components/NavDesktop.tsx
+++ app/components/NavDesktop.tsx
@@ -1,5 +1,6 @@
 'use client';
 import NavDesktopItem from './NavDesktopItem';
+import { usePathname } from 'next/navigation';
 // @ts-ignore
 import Obfuscate from 'react-obfuscate';
 import Link from 'next/link';
@@ -11,18 +12,31 @@ import {
 } from 'react-icons/lia';
 
 export default function NavDesktop() {
+    const pathname = usePathname();
     return (
         <nav id="nav-desktop">
             <ul id="primary">
                 <NavDesktopItem url="/" name="home" />
                 <NavDesktopItem url="/shows" name="shows" />
-                <NavDesktopItem url="/else" name="other stuff we're up to" />
+                <li
+                    className={
+                        pathname == '/else' ? 'nav-item active' : 'nav-item'
+                    }
+                >
+                    <Link href="/else">
+                        other stuff <br />
+                        we're up to
+                    </Link>
+                </li>
                 <li className="nav-item">
                     <span>contact:</span>
+                    <br />
                     <Obfuscate email="b@slownames.net" />
                 </li>
                 <li className="nav-item">
-                    <Link href="https://mailchi.mp/27bd7c689968/slow-names-mailing-list-receive-star-card-receiver-etc">mailing list</Link>
+                    <Link href="https://mailchi.mp/27bd7c689968/slow-names-mailing-list-receive-star-card-receiver-etc">
+                        mailing list
+                    </Link>
                 </li>
                 <li id="nav-item-socials">
                     <ul id="nav-desktop-socials">
