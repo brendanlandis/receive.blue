00aa59b487650605931d909bd464febfd58f91a2
Brendan Landis <b@slownames.net>
2024-08-07 16:36:53 -0400

    updating pkgs, adding editorconfig file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,49 @@
-import './css/screen.scss';
-import type { Metadata } from 'next';
-import NavMobile from './components/NavMobile';
-import NavDesktop from './components/NavDesktop';
+import "./css/screen.scss";
+import type { Metadata } from "next";
+import NavMobile from "./components/NavMobile";
+import NavDesktop from "./components/NavDesktop";
 
 export const metadata: Metadata = {
-    title: 'Receive',
-    description: 'art rock band in Ridgewood',
+  title: "Receive",
+  description: "art rock band in Ridgewood",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" data-theme="lofi">
-            <head>
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/apple-touch-icon.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/favicon-16x16.png"
-                />
-                <link rel="manifest" href="/site.webmanifest" />
-            </head>
-            <body>
-                <div id="background-wrapper">
-                    <header>
-                        <NavDesktop />
-                        <NavMobile />
-                    </header>
-                    {children}
-                </div>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" data-theme="lofi">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body>
+        <div id="background-wrapper">
+          <header>
+            <NavDesktop />
+            <NavMobile />
+          </header>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
 }
--- app/shows/page.tsx
+++ app/shows/page.tsx
@@ -1,25 +1,25 @@
-import { Metadata } from 'next';
-import Header from '../components/Header';
-import UpcomingShows from '../components/UpcomingShows';
-import OldShows from '../components/OldShows';
+import { Metadata } from "next";
+import Header from "../components/Header";
+import UpcomingShows from "../components/UpcomingShows";
+import OldShows from "../components/OldShows";
 
 export const metadata: Metadata = {
-    title: 'Receive :: shows',
+  title: "Receive :: shows",
 };
 
 export default function Shows() {
-    return (
-        <>
-            <main id="shows">
-                <Header />
-                <div className="upcoming">
-                    <UpcomingShows />
-                </div>
-                <div className="previous">
-                    <h2>old show flyers</h2>
-                    <OldShows />
-                </div>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main id="shows">
+        <Header />
+        <div className="upcoming">
+          <UpcomingShows />
+        </div>
+        <div className="previous">
+          <h2>old show flyers</h2>
+          <OldShows />
+        </div>
+      </main>
+    </>
+  );
 }
