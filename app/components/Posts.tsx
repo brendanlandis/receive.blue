75011ef504f6346d2eeb22f2a7640892ef260410
Brendan Landis <b@slownames.net>
2024-10-24 09:55:54 -0400

    package updates & formatting changes

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,65 +1,69 @@
-'use client';
-import useAxios from 'axios-hooks';
-import { Post, RawPostData } from '@/app/types';
-import { format } from 'date-fns/format';
-import { parseISO } from 'date-fns/parseISO';
-import Markdown from 'react-markdown';
+"use client";
+import useAxios from "axios-hooks";
+import { Post, RawPostData } from "@/app/types";
+import { format } from "date-fns/format";
+import { parseISO } from "date-fns/parseISO";
+import Markdown from "react-markdown";
 
 // TODO if the post is about two of our bands, the one that isn't Receive should be added to the otherBands field
 
 export default function Posts() {
-    const [{ data: posts, loading, error }, refetch] = useAxios(
-        'https://api.slownames.net/api/posts?populate=deep&filters[bands][id]=32'
+  const [{ data: posts, loading, error }, refetch] = useAxios(
+    "https://api.slownames.net/api/posts?populate=deep&filters[bands][id]=32"
+  );
+
+  if (loading) return <p>loading</p>;
+  if (error) return <p>error</p>;
+
+  const formatPosts = (posts: { data: RawPostData[] }): Post[] => {
+    const formattedPostsData: Post[] = posts.data.map((post) => ({
+      id: post.id,
+      realDate: post.attributes.date,
+      date: format(parseISO(post.attributes.date), "MMMM do, GGGG yyyy"),
+      headline: post.attributes.headline,
+      text: post.attributes.text,
+      attachments: post.attributes.attachments.map((attachment) => ({
+        id: attachment.id,
+        text: attachment.linkText,
+        url: attachment.file.data
+          ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${attachment.file.data.attributes.url}`
+          : "",
+      })),
+    }));
+
+    formattedPostsData.sort(
+      (a, b) => new Date(b.realDate).getTime() - new Date(a.realDate).getTime()
+    );
+    const currentDate = new Date();
+    const filteredPostsData = formattedPostsData.filter(
+      (post) => new Date(post.realDate) <= currentDate
     );
 
-    if (loading) return <p>loading</p>;
-    if (error) return <p>error</p>;
+    return filteredPostsData;
+  };
 
-    const formatPosts = (posts: { data: RawPostData[] }): Post[] => {
-        const formattedPostsData: Post[] = posts.data.map((post) => ({
-            id: post.id,
-            realDate: post.attributes.date,
-            date: format(parseISO(post.attributes.date), 'MMMM do, GGGG yyyy'),
-            headline: post.attributes.headline,
-            text: post.attributes.text,
-            attachments: post.attributes.attachments.map((attachment) => ({
-                id: attachment.id,
-                text: attachment.linkText,
-                url: attachment.file.data
-                    ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${attachment.file.data.attributes.url}`
-                    : '',
-            })),
-        }));
+  const formattedPosts = posts ? formatPosts(posts) : [];
 
-        formattedPostsData.sort((a, b) => new Date(b.realDate).getTime() - new Date(a.realDate).getTime());
-        const currentDate = new Date();
-        const filteredPostsData = formattedPostsData.filter((post) => new Date(post.realDate) <= currentDate);
-
-        return filteredPostsData;
-    };
-
-    const formattedPosts = posts ? formatPosts(posts) : [];
-
-    return (
-        <>
-            {formattedPosts.map((post: Post) => (
-                <div className="post" key={post.id}>
-                    <div className="post-header">
-                        {post.headline && <h2>{post.headline}</h2>}
-                        <div className="post-date">{post.date}</div>
-                    </div>
-                    <div className="post-text">
-                        <Markdown>{post.text}</Markdown>
-                    </div>
-                    <div className="post-links">
-                        {post.attachments.map((attachment) => (
-                            <a href={attachment.url} key={attachment.id}>
-                                {attachment.text}
-                            </a>
-                        ))}
-                    </div>
-                </div>
+  return (
+    <>
+      {formattedPosts.map((post: Post) => (
+        <div className="post" key={post.id}>
+          <div className="post-header">
+            {post.headline && <h2>{post.headline}</h2>}
+            <div className="post-date">{post.date}</div>
+          </div>
+          <div className="post-text">
+            <Markdown>{post.text}</Markdown>
+          </div>
+          <div className="post-links">
+            {post.attachments.map((attachment) => (
+              <a href={attachment.url} key={attachment.id}>
+                {attachment.text}
+              </a>
             ))}
-        </>
-    );
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
--- app/whatsgoingon/Schedule.tsx
+++ app/whatsgoingon/Schedule.tsx
@@ -1,83 +1,83 @@
-'use client';
-import useAxios from 'axios-hooks';
-import { Show, RawShowData } from '@/app/types';
-import { GetShowDetails } from '../components/GetShowDetails';
+"use client";
+import useAxios from "axios-hooks";
+import { Show, RawShowData } from "@/app/types";
+import { GetShowDetails } from "../components/GetShowDetails";
 
 export default function Schedule() {
-    const [{ data: shows, loading, error }, refetch] = useAxios(
-        'https://api.slownames.net/api/shows?populate=deep&filters[myBand][band][id]=43&filters[myBand][band][id]=34&filters[myBand][band][id]=35&filters[myBand][band][id]=40&filters[myBand][band][id]=32&filters[myBand][band][id]=42&filters[myBand][band][id]=41&publicationState=preview'
+  const [{ data: shows, loading, error }, refetch] = useAxios(
+    "https://api.slownames.net/api/shows?populate=deep&filters[myBand][band][id]=43&filters[myBand][band][id]=34&filters[myBand][band][id]=35&filters[myBand][band][id]=40&filters[myBand][band][id]=32&filters[myBand][band][id]=42&filters[myBand][band][id]=41&publicationState=preview"
+  );
+
+  if (loading) return <p>loading shows...</p>;
+  if (error) return <p>error</p>;
+
+  const formatShows = (shows: { data: RawShowData[] }): Show[] => {
+    return shows.data.map(GetShowDetails);
+  };
+
+  const formattedShows = shows ? formatShows(shows) : [];
+
+  const formatUpcomingShows = (shows: { data: RawShowData[] }): Show[] => {
+    formattedShows.sort(
+      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
     );
+    const currentDate = new Date();
+    const filteredShowsData = formattedShows.filter(
+      (show) => new Date(show.date) >= currentDate
+    );
+    return filteredShowsData;
+  };
 
-    if (loading) return <p>loading shows...</p>;
-    if (error) return <p>error</p>;
+  const upcomingShows = shows ? formatUpcomingShows(shows) : [];
 
-    const formatShows = (shows: { data: RawShowData[] }): Show[] => {
-        return shows.data.map(GetShowDetails);
-    };
-
-    const formattedShows = shows ? formatShows(shows) : [];
-
-    const formatUpcomingShows = (shows: { data: RawShowData[] }): Show[] => {
-        formattedShows.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-        const currentDate = new Date();
-        const filteredShowsData = formattedShows.filter((show) => new Date(show.date) >= currentDate);
-        return filteredShowsData;
-    };
-
-    const upcomingShows = shows ? formatUpcomingShows(shows) : [];
-
-    return (
+  return (
+    <>
+      {upcomingShows.length > 0 ? (
         <>
-            {upcomingShows.length > 0 ? (
-                <>
-                    <ul>
-                        {upcomingShows.map((show: Show) => (
-                            <li key={show.id}>
-                                <span className="date">{show.shortDate} &mdash; </span>
-                                {show.bands.map((band) => (
-                                    <strong key='band.id'>{band.bandname}</strong>
-                                ))}{' '}
-                                {show.venue && <>at {show.venue} </>}
-                                {show.city && <>in {show.city} </>}
-                                {show.otherBands && <>with {show.otherBands} </>}
-                                {show.notes && <>({show.notes})</>}
-                                {(show.eventLinks.length > 0 || show.flyers) && (
-                                    <>
-                                        <span className="links">
-                                            (
-                                            {show.eventLinks.length > 0 && (
-                                                <>
-                                                    {show.eventLinks.map((link) => (
-                                                        <span key={link.id}>
-                                                            <a href={link.url}>
-                                                                {link.text}
-                                                            </a>
-                                                        </span>
-                                                    ))}
-                                                </>
-                                            )}
-                                            {show.flyers && (
-                                                <>
-                                                    {show.flyers.map((flyer) => (
-                                                        <span key={flyer.id}>
-                                                            <a href={flyer.urlLarge}>
-                                                                flyer
-                                                            </a>
-                                                        </span>
-                                                    ))}
-                                                </>
-                                            )}
-                                            )
-                                        </span>
-                                    </>
-                                )}
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            ) : (
-                <>nothing booked wow</>
-            )}
+          <ul>
+            {upcomingShows.map((show: Show) => (
+              <li key={show.id}>
+                <span className="date">{show.shortDate} &mdash; </span>
+                {show.bands.map((band) => (
+                  <strong key="band.id">{band.bandname}</strong>
+                ))}{" "}
+                {show.venue && <>at {show.venue} </>}
+                {show.city && <>in {show.city} </>}
+                {show.otherBands && <>with {show.otherBands} </>}
+                {show.notes && <>({show.notes})</>}
+                {(show.eventLinks.length > 0 || show.flyers) && (
+                  <>
+                    <span className="links">
+                      (
+                      {show.eventLinks.length > 0 && (
+                        <>
+                          {show.eventLinks.map((link) => (
+                            <span key={link.id}>
+                              <a href={link.url}>{link.text}</a>
+                            </span>
+                          ))}
+                        </>
+                      )}
+                      {show.flyers && (
+                        <>
+                          {show.flyers.map((flyer) => (
+                            <span key={flyer.id}>
+                              <a href={flyer.urlLarge}>flyer</a>
+                            </span>
+                          ))}
+                        </>
+                      )}
+                      )
+                    </span>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
         </>
-    );
+      ) : (
+        <>nothing booked wow</>
+      )}
+    </>
+  );
 }
