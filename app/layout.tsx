389098b60bb06c375911decb7b464358a0b55715
Brendan Landis <b@slownames.net>
2024-12-08 16:04:22 -0500

    adding analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./css/screen.scss";
 import type { Metadata } from "next";
 import NavMobile from "./components/NavMobile";
 import NavDesktop from "./components/NavDesktop";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Receive",
@@ -16,6 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="lofi">
       <head>
+        <Script
+          defer
+          src="https://stats.slownames.net/script.js"
+          data-website-id="09bf29c0-9eb1-4d99-9b28-2273f7d5391a"
+        />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
